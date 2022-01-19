6290b0a8dd4330c943969b4211c53e2f3a5eac60
anaximeno <anaximenobrito@gmail.com>
2022-01-19 12:56:41 -0100

    Fix sync and async naming
    The types were changed so I fix it to he right name for the right type

--- asynchronous.c
+++ asynchronous.c
@@ -6,29 +6,32 @@
 #include "common.c"
 
 /* ASYNCHRONOUS Concurrent Philosophers Version:
- * In the asynchronous version, the philosopher who didn't get one of the chopsticks
- * will wait until the chopstick is released to eat and then think.
+ * In the asynchronous version, the philosopher who didn't get the two chopsticks
+ * will stop eating and go straight to think, instead of waiting until the other chopstick is released to eat
+ * and only then thinking as in synchronous.
  */
-
 
 void* action(void* __phil) {
     phil_t* phil = (phil_t*) __phil;
+    long left, right;
 
     do {
         #if SHOW_INTENTION
             printf(" + %s is trying to eat!\n\n", phil->name);
         #endif
 
-        pthread_mutex_lock(phil->left_hashi);
-        pthread_mutex_lock(phil->right_hashi);
+        left = pthread_mutex_trylock(phil->left_hashi);
+        right = pthread_mutex_trylock(phil->right_hashi);
 
-        eat(rand() % 6 + 3, phil);
+        if (!left && !right) {
+            eat(rand() % 6 + 3, phil);
+        }
 
         pthread_mutex_unlock(phil->left_hashi);
         pthread_mutex_unlock(phil->right_hashi);
 
         think(rand() % 7 + 2, phil);
-    } while(!halt);
+    } while (!halt);
 
     pthread_exit(NULL);
 }
